0cb40a6d7f5212b2fd2f010c777a1172f403f768
Ronald Hensbergen <ronaldhensbergen@gmail.com>
2025-09-08 21:42:42 +0200

    small changes

--- Python/first_attempt.py
+++ Python/first_attempt.py
@@ -1,21 +1,40 @@
-import psycopg as pg
-import pandas as pd
 from sqlalchemy import create_engine
+from pandas import read_sql_query
 from Helpers.connectors import pg_connection_string
 
 # Create engine
 engine = create_engine(pg_connection_string)
 
-query = "SELECT * FROM vw_current_situation"
-df_current_situation = pd.read_sql_query(query, engine)
-query = 'SELECT * FROM vw_user_bonus_preferences'
-df_preferences = pd.read_sql_query(query, engine)
-query = """
+qr_current_situation = "SELECT * FROM vw_current_situation"
+qr_preferences = 'SELECT * FROM vw_user_bonus_preferences'
+qr_selection = """
 	(select id AS wonder_id from wonders where allied = false limit 4)
 union
 	(select id from wonders where allied = true limit 4)
 """
-df_selection = pd.read_sql_query(query, engine)
-print(df_current_situation)
-print(df_preferences)
-print(df_selection)
+qr_wonders = "SELECT * FROM vw_wonders"
+
+df_current_situation = read_sql_query(qr_current_situation, engine)
+df_preferences = read_sql_query(qr_preferences, engine)
+df_selection = read_sql_query(qr_selection, engine)
+df_wonders = read_sql_query(qr_wonders, engine)
+
+df_current_selection = (
+    df_current_situation[df_current_situation['wonder_id']
+                         .isin(df_selection['wonder_id'])]
+                         )
+df_wonders_selection = (
+    df_wonders[df_wonders['id']
+			   .isin(df_selection['wonder_id'])]
+)
+
+print("""Current situation
+""",df_current_situation)
+print("""User preferences
+""",df_preferences)
+# print("""Random selection
+# """,df_selection)
+print("""Wonders selection
+""",df_wonders_selection)
+# print(df_current_selection)
+# print(df_preferences.dtypes)
--- README.md
+++ README.md
@@ -2,10 +2,14 @@ This project is meant to help players of Rise of Cultures to select the best won
 It can be quite complex, because it depends on several parameters:
 - Wonder levels
 - Player play style and preferences
-- Synergy between wonders
+- The wonder type
+- The total amount of the synergy wonder type placed in the grid
+- The synergy multiplier (2 for Yggdrasil and Dragonship Ellida)
 
 This project currently is:
 - A PostgreSQL database
 - A Python project
 
+
 This project has just started!
+
